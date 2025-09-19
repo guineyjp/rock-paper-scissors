11b350f0c29f5fbb461e2ddbd09d0dcde18de73d
John <guineyjp@gmail.com>
2025-09-19 19:28:11 +0100

    Created computer side of the rock paper scissors game

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Hello Worlds");
+
+function getComputerChoice() {
+    const randomComputerResult = Math.random();
+
+    if (randomComputerResult <= 0.33) {
+        return "rock";
+    } else if (randomComputerResult >0.33 && randomComputerResult <=0.66) {
+        return "paper";
+    }
+     else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
+
+function randomResultSimple() {
+    const randomResult2 = Math.random();
+
+if (randomResult2 <=0.33) {
+    return "rock";
+} else {
+    return "scissors";
+}
+}
+
+console.log(randomResultSimple());
